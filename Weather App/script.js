4c635f5c9b010acec870e91d1007fcfb377fbc8b
T Rajeev <telagasettirajeev999@gmail.com>
2024-10-07 00:20:54 +0530

    changes done

--- Weather App/script.js
+++ Weather App/script.js
@@ -26,7 +26,8 @@ const dayForcastTime1 = document.querySelector('#dayForcastTime1');
 const dayForcastTime2 = document.querySelector('#dayForcastTime2');
 const dayForcastTime3 = document.querySelector('#dayForcastTime3');
 
-let apiId = window.config.API_KEY;
+// let apiId = window.config.API_KEY;
+let apiId = "4d3b54b7a4ece628ad84a0428d8c9c2c";
 console.log("my apiId is :" , apiId);
 
 async function getWeather(city) {
